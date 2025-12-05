90affde89787b7b581b2589c70c284c54b75fb1a
Kauan Leardini Garcia <kauanlear12.garcia@gmail.com>
2025-12-05 12:51:12 -0300

    nova mensagem

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o gitHub");
 turboLogger("Estou aprendendo o fluxo do git");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
