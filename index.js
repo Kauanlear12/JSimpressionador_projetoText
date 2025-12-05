bfa8435366d2842dbd6440d08e2fd984c218745b
Kauan Leardini Garcia <kauanlear12.garcia@gmail.com>
2025-12-05 12:25:51 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o gitHub");
 turboLogger("Estou aprendendo o fluxo do git");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão prncipal do sistema",
+  "darkgreen"
+);
