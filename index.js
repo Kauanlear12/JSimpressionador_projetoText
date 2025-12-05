2be7af13852d1cc83ff655b060cba8c46b784cf9
Kauan Leardini Garcia <kauanlear12.garcia@gmail.com>
2025-12-05 12:49:33 -0300

    Resolvendo

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 import { turboLogger } from "./turboLogger.js";
 
-console.log("teste");
 console.log("Estou usando o gitHub");
 turboLogger("Estou aprendendo o fluxo do git");
-turboLogger(
-  "Eu consegui criar uma nova mensagem sem afetar a versão prncipal do sistema",
-  "darkgreen"
-);
